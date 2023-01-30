8fb7992a4883ea43975e3f17bc0ce90bacea4c4c
Ivan Volko <iwolkkov@gmail.com>
2023-01-30 20:40:43 +0200

    getters and setters added

--- src/main/java/org/example/model/SportCar.java
+++ src/main/java/org/example/model/SportCar.java
@@ -2,4 +2,12 @@ package org.example.model;
 
 public class SportCar  extends Car{
     private int spoilers;
+
+    public int getSpoilers() {
+        return spoilers;
+    }
+
+    public void setSpoilers(int spoilers) {
+        this.spoilers = spoilers;
+    }
 }
